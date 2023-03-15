e8bfaed46c8a40707051c262990a487539032c84
ramon284 <ramon.cremers@hotmail.com>
2023-03-15 09:54:11 +0100

    Face tracking persistence improved

--- face_tracking_persistence.py
+++ face_tracking_persistence.py
@@ -6,20 +6,24 @@ import os
 import cv2
 import pickle
 import time
+import numpy as np
+import pandas as pd
 
 KNOWN_FACES = "known_faces"
 UNKNOWN_FACES = "unknown_faces"
-TOLERANCE = 0.5
+TOLERANCE = 0.54 ## <- most important hyperparameter. Lower means it more quickly recognizes a "new" face
 FRAME_THICCNESS = 3
 FONT_THICCNESS = 2
 MODEL = 'cnn' 
 
-video = cv2.VideoCapture('Data/Video/Video0.mp4')
+video = cv2.VideoCapture('Data/Video/Video0_Trim.mp4')
 print(video.isOpened())
 
 known_faces = []
 known_names = []
 for name in os.listdir(KNOWN_FACES):
+    if name == '.gitignore':
+        continue
     for filename in os.listdir(f"{KNOWN_FACES}/{name}"):
         #image = face_recognition.load_image_file(f"{KNOWN_FACES}/{filename}")
         #encoding = face_recognition.face_encodings(image)[0]
@@ -32,11 +36,18 @@ if len(known_names) > 0:
 else:
     next_id = 0
 
+faceData = pd.DataFrame(columns=['Frame','Person', 'FaceRectX', 'FaceRectY', 'FaceRectWidth', 'FaceRectHeight'])
+frame_i = 0
 while True:
     ret, image = video.read()
+    if frame_i % 3 == 0:
+        frame_i += 1
+        continue
+    if ret == False:
+        break
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    locations = face_recognition.face_locations(image, number_of_times_to_upsample=0,  model='hog')
-    encodings = face_recognition.face_encodings(image, locations)
+    locations = face_recognition.face_locations(image, number_of_times_to_upsample=0,  model='cnn')
+    encodings = face_recognition.face_encodings(image, locations, model='large')
 
     for face_encoding, face_location in zip(encodings, locations):
         results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
@@ -51,18 +62,29 @@ while True:
             os.mkdir(f"{KNOWN_FACES}\{match}")
             pickle.dump(face_encoding, open(f"{KNOWN_FACES}\{match}\{match}-{int(time.time())}.pkl", 'wb'))
 
-        top_left = (face_location[3], face_location[0])
-        bottom_right = (face_location[1], face_location[2])
-        color = [0, 255, 0]
-        cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICCNESS)
-        top_left = (face_location[3], face_location[2])
-        bottom_right = (face_location[1], face_location[2]+22)
-        cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
-        cv2.putText(image, str(match), (face_location[3]+10, face_location[2]+15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (200,200,200))
+        ## This is for visualizing the bounding boxes in real-time, with annotations
+        
+        # top_left = (face_location[3], face_location[0])
+        # bottom_right = (face_location[1], face_location[2])
+        # color = [0, 255, 0]
+        # cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICCNESS)
+        # top_left = (face_location[3], face_location[2])
+        # bottom_right = (face_location[1], face_location[2]+22)
+        # cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+        # cv2.putText(image, str(match), (face_location[3]+10, face_location[2]+15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (200,200,200))
+        top, right, bottom, left = face_location
+        faceData = pd.concat([faceData, 
+                            pd.DataFrame({'Frame': frame_i, 'Person': str(match), 'FaceRectX': left, 'FaceRectY': top, 
+                                          'FaceRectWidth': right - left, 'FaceRectHeight': bottom - top}, 
+                                         index=[0])], 
+                            ignore_index=True)
 
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    cv2.imshow('x', image)
-    if cv2.waitKey(1) & 0xFF ==ord('e'):
-        break
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # cv2.imshow('x', image)
+    # if cv2.waitKey(1) & 0xFF ==ord('e'):
+    #     break
+    frame_i += 1
+    
 video.release()
 cv2.destroyAllWindows()
+faceData.to_csv('facial_persistence_test.csv', index=False)
